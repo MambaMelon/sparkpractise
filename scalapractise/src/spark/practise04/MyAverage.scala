df953370b7c26a808608966732ede0c523032639
MambaMelon <1115431474@qq.com>
2017-12-19 19:27:12 +0800

    用户自定义类型安全聚合函数

---
+++ scalapractise/src/spark/practise04/MyAverage.scala
@@ -0,0 +1,48 @@
+package spark.practise04
+
+import org.apache.spark.sql.{Encoder, Encoders, Row, SparkSession}
+import org.apache.spark.sql.expressions.{Aggregator, MutableAggregationBuffer, UserDefinedAggregateFunction}
+import org.apache.spark.sql.types._
+
+/**
+  * 用户定义类型安全的函数
+  * Created by admin on 2017/12/19.
+  */
+
+
+case class Employee(name: String, salary: Long)
+case class Average(var sum: Long, var count: Long)
+object MyAverage extends Aggregator[Employee, Average, Double] {
+
+
+  override def zero: Average = Average(0L, 0L)
+
+  override def reduce(buffer: Average, employee: Employee): Average = {
+    buffer.sum += employee.salary
+    buffer.count += 1
+    buffer
+  }
+
+  override def merge(aver1: Average, aver2: Average): Average = {
+    aver1.sum += aver2.sum
+    aver1.count += aver2.count
+    aver1
+  }
+
+  override def finish(reduction: Average): Double = {
+    reduction.sum.toDouble / reduction.count
+  }
+
+  override def bufferEncoder: Encoder[Average] = Encoders.product
+
+  override def outputEncoder: Encoder[Double] = Encoders.scalaDouble
+
+
+  def main(args: Array[String]): Unit = {
+    val sparkSession = SparkSession.builder().master("local").appName("MysqlOperation").getOrCreate()
+    val ds = sparkSession.read.json("D://employees.json")
+    val averageSalary = MyAverage.toColumn.name("average_salary")
+    val result = ds.select(averageSalary)
+    result.show()
+  }
+}
