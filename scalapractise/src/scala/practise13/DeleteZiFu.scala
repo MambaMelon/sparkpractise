02b7bb9de13d03a1f73ea08e88c7a6981fcacbc6
MambaMelon <1115431474@qq.com>
2017-12-24 23:35:47 +0800

    删除公共字符

---
+++ scalapractise/src/scala/practise13/DeleteZiFu.scala
@@ -0,0 +1,12 @@
+package scala.practise13
+
+/**
+  * 删除公共字符串
+  * Created by Melon on 2017/12/24.
+  */
+object DeleteZiFu extends App {
+  val str = "They are students"
+  val word = "aeiou"
+  val newStr = str.replaceAll("[" + word.toSet + "]", "")
+  println(newStr)
+}
