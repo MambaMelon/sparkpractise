ed04731de8c6dcf466c2a40def99dae7cfe2070b
MambaMelon <1115431474@qq.com>
2017-12-27 17:50:47 +0800

    scala api 练习

--- scalapractise/src/scala/practise05/Api01.scala
+++ scalapractise/src/scala/practise05/Api01.scala
@@ -1,5 +1,7 @@
 package scala.practise05
 
+import scala.collection.GenSeq
+
 /**
   * Created by Melon on 2017/12/27.
   */
@@ -40,6 +42,17 @@ object Api01 extends App {
   //flatten 【f1:List(1, 2, 3, 1, 2, 3)】【f2:Set(1, 2, 3)】
   val f1 = List(Set(1, 2, 3), Set(1, 2, 3)).flatten
   val f2 = Set(List(1, 2, 3), List(3, 2, 1)).flatten
+  //zipWithIndex 【List((1,0), (2,1), (3,2), (4,3))】
+  val l12 = l.zipWithIndex
+  //zipAll 【如果l长度较短，则用x填充；如果l较长，则用y填充】
+  val lx = List("a", "b", "c", "d", "e")
+  val x = 0
+  val y = '_'
+  val l13 = l.zipAll(lx, x, y)
+  //lastIndexOfSlice  【没有匹配结果则返回-1】
+  val l14 = l.lastIndexOfSlice(GenSeq[Int](1, 5), 0)
+  //
+  val l15 = l.isDefinedAt(3)
 
-  l.par.foreach(println)
+  println(l15)
 }
