7eccf05c946f14b0f7146a48d92666b112944cae
MambaMelon <1115431474@qq.com>
2018-03-15 11:42:33 +0800

    val var与可变集合 不可变集合

--- scalapractise/src/scala/practise14/Test02.scala
+++ scalapractise/src/scala/practise14/Test02.scala
@@ -3,6 +3,19 @@ package scala.practise14
 /**
   * Created by Melon on 2017/12/29.
   */
+
+class Foo
+trait TypeHolder[T] {
+  type ElementType = T
+}
+class FooTypeHolder extends TypeHolder[Foo]
+class Client[T <: TypeHolder[_]](val holder: T) {
+  def showElementType(t: T#ElementType): Unit = {
+    println("show element type " + t.toString)
+    println(t.getClass)
+  }
+}
+
 object Test02 extends App {
 
 //  val list = List[(Int, Int)]((9, 4), (1, 5), (4, 6))
@@ -10,13 +23,36 @@ object Test02 extends App {
 //    case (a,b) => a * b
 //  }).sum
 
-  def unapplySeq(email: String):
-    Option[(String, Seq[String])] = {
-    val parts = email split "@"
-    if(parts.length == 2)
-      Some(parts(0), parts(1).split("\\.").reverse)
-    else
-      None
-  }
+//  def unapplySeq(email: String):
+//    Option[(String, Seq[String])] = {
+//    val parts = email split "@"
+//    if(parts.length == 2)
+//      Some(parts(0), parts(1).split("\\.").reverse)
+//    else
+//      None
+//  }
+
+//    private val foo = new Foo
+//    private val holder = new FooTypeHolder
+//    private val client = new Client(holder)
+//    client.showElementType(foo)
+
+  //val和var修饰可变对象与不可变对象
+  val m1 = scala.collection.mutable.Map(('a',1), ('b', 2))
+  //可以修改
+  m1 += (('c', 3))
+  println(m1)
+
+  //但是不能指向其它对象，会报错
+  //map = scala.collection.mutable.Map(('a',1), ('b', 2))
+
+  var m2 = scala.collection.mutable.Map(('a',1), ('b', 2))
+  m2 += (('c', 3))
+  println(m2)
+
+
+
+
 
 }
+
