03e3c1c2c5ee2153b7061cb2d314c8afa357246c
MambaMelon <1115431474@qq.com>
2018-03-28 11:25:46 +0800

    移动文件

--- scalapractise/src/spark/practise04/SparkStreaming.scala
+++ scalapractise/src/spark/practise04/SparkStreaming.scala
@@ -1,8 +1,8 @@
-package spark.practise05
+package spark.practise04
 
+import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.collection.mutable
 
