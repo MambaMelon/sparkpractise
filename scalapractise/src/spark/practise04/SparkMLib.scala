84d93bb77f24d53c06f1542172c267c8fe3e2e62
MambaMelon <1115431474@qq.com>
2018-04-10 06:52:39 +0800

    spark MLib用户推荐电影

---
+++ scalapractise/src/spark/practise04/SparkMLib.scala
@@ -0,0 +1,35 @@
+package spark.practise04
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.recommendation.{ALS, Rating}
+/**
+  * SparkMLib操作
+  * Created by admin on 2017/04/10.
+  */
+object SparkMLib {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("SparkMLib").setMaster("local")
+    val sc = new SparkContext(conf)
+    //用户ID  影片ID  星级  时间戳
+    val rawData = sc.textFile("F:\\ml-100k\\u.data")
+    //模型训练时不需要时间戳，故过滤
+    val rawRatings = rawData.map(_.split("\t").take(3))
+    //将用户ID、影片ID和星级转换为Rating对象
+    val ratings = rawRatings.map({case Array(user, product, rating) =>
+    Rating(user.toInt, product.toInt, rating.toDouble)})
+    //训练推荐模型
+    val model = ALS.train(ratings, 50, 10, 0.01)
+    //预测用户789对电影242的评级为predictedRating
+    val predictedRating = model.predict(196, 242)
+    //推荐用户789的感兴趣的5个电影
+//    val topKRecs = model.recommendProducts(789, 5)
+    //检验推荐
+    //导入电影数据 电影ID 电影标题
+    val movies = sc.textFile("F:\\ml-100k\\u.item")
+    val titles = movies.map(line => line.split("\\|").take(2)).
+      map(array => (array(0).toInt, array(1))).collectAsMap()
+    //找出用户789评级最高的前5部电影
+    val moviesForUser = ratings.keyBy(_.user).lookup(789)
+    println(moviesForUser.size)
+  }
+}
